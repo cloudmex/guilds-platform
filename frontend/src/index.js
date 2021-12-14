56f2ba9795e6f0f484db090efa10dff06c36087d
Cristian Zambrano <cristianzambrano@MacBook-Pro-de-Cristian.local>
2021-12-13 19:16:58 -0700

    check css problems

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,7 +20,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "assets/css/nucleo-icons.css";
-import "assets/scss/blk-design-system-react.scss?v=1.2.0";
+import "assets/scss/blk-design-system-react.scss";
 import "assets/demo/demo.css";
 
 import App from "./App.js";
